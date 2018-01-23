08d391e39c28fcf20f1512d11542f208aece99a1
Antonio Rafael Pontiveros Calzado <arpontiveros@atsistemas.com>
2018-01-23 10:02:03 +0100

    first commit

---
+++ 01_SpringMvc/src/main/java/com/curso/spring/mvc/config/ConfiguracionWeb.java
@@ -0,0 +1,11 @@
+package com.curso.spring.mvc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+@EnableWebMvc 
+public class ConfiguracionWeb extends WebMvcConfigurationSupport{
+
+}
